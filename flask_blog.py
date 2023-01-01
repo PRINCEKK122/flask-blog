0323374cc57a23ad8d6ef75e39b1eeb2b953e4cb
PRINCEKK122 <awuahkarikari12@gmail.com>
2023-01-01 22:45:12 +0000

    added templates directory to render a basic html for home and about pages

--- flask_blog.py
+++ flask_blog.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Prince Awuah Karikari",
+        "title": "Blog 1",
+        "content": "First content",
+        "date_posted": "January 1, 2023",
+    },
+    {
+        "author": "John Doe",
+        "title": "Blog 2",
+        "content": "Second content",
+        "date_posted": "April 21, 2018",
+    },
+]
 
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html", title="About")
 
 
 if __name__ == "__main__":
