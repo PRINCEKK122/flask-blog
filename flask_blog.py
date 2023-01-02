c1fd468092e2be74663f3e855628a349c15d4862
PRINCEKK122 <awuahkarikari12@gmail.com>
2023-01-02 23:55:37 +0000

    created register and login pages for both back to front

--- flask_blog.py
+++ flask_blog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "eaace66504036762549d64aca993ef2b"
 
 posts = [
     {
@@ -28,6 +30,20 @@ def hello():
 def about():
     return render_template("about.html", title="About")
 
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("hello")) # url_for uses the name of the method not the string argument in the app.route() decorator
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
